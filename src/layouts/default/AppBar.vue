3161e6b047d59396b7283e02a1827d05974a3d9c
Siphamandla Mzobe <siphamandla.mzobe@kcpivot.co.za>
2023-08-23 12:05:28 +0200

    added a new layout

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,54 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-4 "
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
 
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            flat
+            hide-details
+            label="Search"
+            rounded="lg"
+            single-line
+            variant="solo-filled"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
 </template>
 
 <script setup>
-  //
+  const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+  ]
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
 </script>
--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,7 +1,39 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                  :title="`List Item ${n}`"
+                ></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  color="grey-lighten-4"
+                  link
+                  title="Refresh"
+                ></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+            <router-view />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
 </template>
 
 <script setup>
